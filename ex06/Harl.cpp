ff2d0b5d22624cd61b9e37a7934c4ac59a7093c4
Serhii Veretennikov <gridis72@gmail.com>
2025-04-25 13:46:59 +0200

    feat: implemented exercise

---
+++ ex06/Harl.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 12:17:01 by svereten          #+#    #+#             */
+/*   Updated: 2025/04/25 13:40:55 by svereten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "Harl.hpp"
+#include <iostream>
+
+Harl::Harl() {};
+
+void Harl::complain(std::string level) {
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	levelFunc	functions[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	int			i = 0;
+
+	while (i < 4) {
+		if (levels[i] == level) {
+			break ;
+		}
+		i++;
+	}
+
+	switch (i) {
+		case 0:
+			std::cout << "[ " << levels[0] << " ]\n";
+			(this->*functions[0])();
+		case 1:
+			std::cout << "[ " << levels[1] << " ]\n";
+			(this->*functions[1])();
+		case 2:
+			std::cout << "[ " << levels[2] << " ]\n";
+			(this->*functions[2])();
+		case 3:
+			std::cout << "[ " << levels[3] << " ]\n";
+			(this->*functions[3])();
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]\n";
+	}
+}
+
+void Harl::debug(void) {
+  std::cout << "I love having extra bacon for my "
+               "7XL-double-cheese-triple-pickle-specialketchup burger. I "
+               "really do!\n";
+}
+
+void Harl::info(void) {
+  std::cout << "I cannot believe adding extra bacon costs more money. You "
+               "didn’t put enough bacon in my burger! If you did, I wouldn’t "
+               "be asking for more!\n";
+}
+
+void Harl::warning(void) {
+  std::cout << "I think I deserve to have some extra bacon for free. I’ve been "
+               "coming for years, whereas you started working here just last "
+               "month.\n";
+}
+
+void Harl::error(void) {
+  std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
---
+++ ex06/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 12:17:07 by svereten          #+#    #+#             */
+/*   Updated: 2025/04/25 13:36:42 by svereten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <string>
+
+class Harl {
+public:
+	Harl();
+	void	complain(std::string level);
+
+private:
+	void		debug(void);
+	void		info(void);
+	void		warning(void);
+	void		error(void);
+};
+
+typedef void	(Harl::*levelFunc)(void);
+
+#endif // !HARL_HPP
