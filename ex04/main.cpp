a2948c606aa33aae7253f797ca357657e8706117
Serhii Veretennikov <gridis72@gmail.com>
2025-04-28 15:13:46 +0200

    fix: now not assigning something that might not exist to needle

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 17:34:33 by svereten          #+#    #+#             */
-/*   Updated: 2025/04/25 12:12:17 by svereten         ###   ########.fr       */
+/*   Updated: 2025/04/28 15:03:45 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <iostream>
@@ -18,11 +18,12 @@ int	main(int argc, char **argv) {
 	std::ofstream	outfile;
 	std::string		fileContent;
 	std::string		s;
-	std::string		needle = std::string(argv[2]);
+	std::string		needle;
 	size_t			needle_idx;
 
 	if (argc != 4)
 		return (1);
+	needle = argv[2];
 	inputfile.open(argv[1]);
 	while (std::getline(inputfile, s)) {
 		fileContent += s;
