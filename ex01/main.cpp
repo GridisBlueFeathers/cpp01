c0fdb1a28cf53dadf37b9831f2998d365fe46fed
Serhii Veretennikov <gridis72@gmail.com>
2025-04-28 14:54:45 +0200

    chore: cleanup

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 15:16:05 by svereten          #+#    #+#             */
-/*   Updated: 2025/04/21 15:39:56 by svereten         ###   ########.fr       */
+/*   Updated: 2025/04/28 14:53:32 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Zombie.hpp"
@@ -17,6 +17,8 @@ int	main(void) {
 	Zombie	*horde = zombieHorde(5, "Jeff");
 	int		i = 0;
 
+	if (!horde)
+		return (1);
 	while (i < 5) {
 		horde[i].announce();
 		i++;
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 15:16:13 by svereten          #+#    #+#             */
-/*   Updated: 2025/04/21 15:33:41 by svereten         ###   ########.fr       */
+/*   Updated: 2025/04/28 14:53:15 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Zombie.hpp"
@@ -17,8 +17,7 @@ Zombie	*zombieHorde(int N, std::string name) {
 
 	if (!res)
 		return (NULL);
-	while (i < N)
-	{
+	while (i < N) {
 		res[i].set_name(name);
 		i++;
 	}
