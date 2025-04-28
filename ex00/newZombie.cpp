d7fd9ccb59430f9c1b2668b42fef0852f9ff24e9
Serhii Veretennikov <gridis72@gmail.com>
2025-04-28 14:17:45 +0200

    chore: cleanup

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,14 +6,11 @@
 /*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:53:24 by svereten          #+#    #+#             */
-/*   Updated: 2025/04/21 13:54:43 by svereten         ###   ########.fr       */
+/*   Updated: 2025/04/28 14:16:15 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Zombie.hpp"
-#include <iostream>
 
 Zombie	*newZombie(std::string name) {
-	Zombie	*res = new Zombie(name);
-
-	return (res);
+	return (new Zombie(name));
 }
--- ex00/randomChump.cpp
+++ ex00/randomChump.cpp
@@ -6,11 +6,10 @@
 /*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:48:54 by svereten          #+#    #+#             */
-/*   Updated: 2025/04/21 13:50:28 by svereten         ###   ########.fr       */
+/*   Updated: 2025/04/28 14:16:30 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Zombie.hpp"
-#include <iostream>
 
 void	randomChump(std::string name) {
 	Zombie	random_chump(name);
