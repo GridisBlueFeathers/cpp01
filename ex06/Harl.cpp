a00efee172c3d21612f10e48bf8c7319762d188b
Serhii Veretennikov <gridis72@gmail.com>
2025-04-28 15:22:36 +0200

    chore: cleanup

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 12:17:01 by svereten          #+#    #+#             */
-/*   Updated: 2025/04/25 13:40:55 by svereten         ###   ########.fr       */
+/*   Updated: 2025/04/28 15:17:54 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Harl.hpp"
@@ -46,23 +46,23 @@ void Harl::complain(std::string level) {
 }
 
 void Harl::debug(void) {
-  std::cout << "I love having extra bacon for my "
-               "7XL-double-cheese-triple-pickle-specialketchup burger. I "
-               "really do!\n";
+	std::cout << "I love having extra bacon for my "
+				"7XL-double-cheese-triple-pickle-specialketchup burger. I "
+				"really do!\n";
 }
 
 void Harl::info(void) {
-  std::cout << "I cannot believe adding extra bacon costs more money. You "
-               "didn’t put enough bacon in my burger! If you did, I wouldn’t "
-               "be asking for more!\n";
+	std::cout << "I cannot believe adding extra bacon costs more money. You "
+				"didn’t put enough bacon in my burger! If you did, I wouldn’t "
+				"be asking for more!\n";
 }
 
 void Harl::warning(void) {
-  std::cout << "I think I deserve to have some extra bacon for free. I’ve been "
-               "coming for years, whereas you started working here just last "
-               "month.\n";
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been "
+				"coming for years, whereas you started working here just last "
+				"month.\n";
 }
 
 void Harl::error(void) {
-  std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
 }
